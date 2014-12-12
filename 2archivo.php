6642368850d95f92da40648fc1b74b16b654a06f
Eliezer <Eliezer@Portatil>
2014-12-12 15:42:02 +0100

    segunda version

---
+++ 2archivo.php
@@ -0,0 +1,17 @@
+
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // test por grupo de un repositorio git
+            echo ("HOLA MUNDO");
+             echo ("segundo commit Eliezer");
+
+     
+        ?>
+    </body>
+</html>
